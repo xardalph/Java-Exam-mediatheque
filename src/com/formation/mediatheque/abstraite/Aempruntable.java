769e84a28fe41ce045851caed2ed41d8c0e81e8e
evan <evan.adam@yahoo.fr>
2021-01-21 22:07:02 +0100

    add get/set borrow

--- src/com/formation/mediatheque/abstraite/Aempruntable.java
+++ src/com/formation/mediatheque/abstraite/Aempruntable.java
@@ -2,4 +2,12 @@ package com.formation.mediatheque.abstraite;
 
 public abstract class Aempruntable extends commonEntity {
     private boolean borrow;
+
+    public boolean isBorrow() {
+        return borrow;
+    }
+
+    public void setBorrow(boolean borrow) {
+        this.borrow = borrow;
+    }
 }
